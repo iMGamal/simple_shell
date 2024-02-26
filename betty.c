b820a3fc2afaa7c137c44d77cb740876c8652f78
iMGamal <mohamed.gamal126027@gmail.com>
2024-02-26 01:36:24 +0000

    first task

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ * Description - program that prints Hello, World
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	printf("Hello, World\n");
+	return (0);
+}
